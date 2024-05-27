ba9acfb7d27b6b6fab51ef2415677422cd97c0f5
SamPrithesh <samuelprithesh@gmail.com>
2024-05-27 10:32:47 +0530

    hii

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("what is the boyfriends name");
+        String name1=scan.next();
+
+        System.out.println("who is" + name1+"s gfs name");
+        String name2 = scan.next();
+
+
+        System.out.println("Awwwwwwwww,they both(" + name1 +"❤️"+ name2 +") are a couple");
+        String name3 = scan.next();
+
+        System.out.println("do you wanna know something");
+        String name3 = scan.next();
+
+        System.out.println("");
+
+
+    }
+}
+
+
+
+
+
+
